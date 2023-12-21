7128098e6600f48d1a819975135e8ab5e7445637
Quoc Thoai <thoaifamily83@gmail.com>
2023-12-21 14:53:16 +0700

    init project

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const publicRoutes = [
+  { path: '/', component: '', layout: '' },
+  { path: '/', component: '', layout: '' },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
