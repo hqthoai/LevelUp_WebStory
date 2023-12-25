c1c391f6a6e3fa135e857512d9ab565440e5c622
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-26 00:58:53 +0700

    make team memebers ui

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import ProductionCard from '../components/ProductionCard/ProductionCard';
 import GameComing from '../components/GameComming/GameComing';
+import TeamMembers from '../components/TeamMembers/TeamMembers';
 function HomePage() {
   return (
     <div className="container bg-slate-800">
       <ProductionCard />
       <GameComing />
+      <TeamMembers/>
     </div>
   );
 }
