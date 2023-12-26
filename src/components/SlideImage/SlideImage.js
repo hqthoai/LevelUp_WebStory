fd823b4044dbc4cee4b38c7d9b745f004520d024
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-26 17:51:33 +0700

    prepare game url to link to game

--- src/components/SlideImage/SlideImage.js
+++ src/components/SlideImage/SlideImage.js
@@ -8,9 +8,10 @@ import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 const slides = [
   {
     id: 1,
-
+    
     imageUrl:
       'https://images.unsplash.com/photo-1500964757637-c85e8a162699?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww',
+      gameUrl:''
   },
   {
     id: 2,
@@ -37,73 +38,6 @@ const slides = [
       'https://images.unsplash.com/photo-1618430279534-323d2c5c7707?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ymx1ZSUyMGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D',
   },
 ];
-
-// function SlideImage() {
-//     const [index, setIndex] = useState(0);
-//     const [currentImages, setCurrentImages] = useState([0, 1, 2]);
-  
-//     // back to the previous image
-//     const handleGoToPrevImage = () => {
-//       const firstImageIndex = currentImages[0];
-//       const prevImageIndex = (firstImageIndex - 1 + slides.length) % slides.length;
-//       setCurrentImages((prevImages) => [prevImageIndex, ...prevImages.slice(0, -1)]);
-//     };
-  
-//     // next image
-//     const handleGoToNextImage = () => {
-//       const lastImageIndex = currentImages[currentImages.length - 1];
-//       const nextImageIndex = (lastImageIndex + 1) % slides.length;
-//       setCurrentImages((prevImages) => [...prevImages.slice(1), nextImageIndex]);
-//     };
-  
-//     return (
-//       <div className="relative w-full min-h-screen overflow-hidden mt-20">
-//         <div className="w-full pt-[50px] flex overflow-hidden">
-//           <div className="flex items-center">
-//             {currentImages.map((i, currentIndex) => (
-//               // Use parentheses to wrap the JSX and return it
-//               currentIndex === 1 ? (
-//                 <div
-//                   key={slides[i].id}
-//                   className="w-[680px] h-[430px] rounded-[10px] overflow-hidden flex justify-center items-center shadow-[0 15px 50px rgba(0, 0, 0, 0.2)] mr-10 -translate-y-8 cursor-pointer"
-//                 >
-//                   <img
-//                     src={slides[i].imageUrl}
-//                     alt={slides[i].id}
-//                     className="object-cover w-full h-full"
-//                   />
-//                 </div>
-//               ) : (
-                
-//                 <div
-//                   key={slides[i].id}
-//                   className="w-[680px] h-[430px] rounded-[10px] overflow-hidden flex justify-center items-center shadow-[0 15px 50px rgba(0, 0, 0, 0.2)] mr-10 translate-y-8"
-//                 >
-//                   <img
-//                     src={slides[i].imageUrl}
-//                     alt={slides[i].id}
-//                     className="object-cover w-full h-full"
-//                   />
-//                 </div>
-//               )
-//             ))}
-//           </div>
-//         </div>
-  
-//         <Box className="flex justify-center mt-[-32px]">
-//           <IconButton>
-//             <ArrowCircleLeftIcon sx={{ color: 'white', mr: 34 }} onClick={handleGoToPrevImage} />
-//           </IconButton>
-//           <IconButton>
-//             <ArrowCircleRightIcon sx={{ color: 'white' }} onClick={handleGoToNextImage} />
-//           </IconButton>
-//         </Box>
-//       </div>
-//     );
-//   }
-  
-//   export default SlideImage;
-  
 
 function SlideImage() {
   const [index, setIndex] = useState(0);
@@ -134,7 +68,7 @@ function SlideImage() {
                 currentIndex === 1
                   ? 'w-[680px] h-[430px] cursor-pointer -translate-y-8'
                   : 'w-[340px] h-[430px] translate-y-8'
-              } rounded-[10px] overflow-hidden flex justify-center items-center shadow-[0 15px 50px rgba(0, 0, 0, 0.2)] mr-10 `}
+              } overflow-hidden flex justify-center items-center shadow-[0 15px 50px rgba(0, 0, 0, 0.2)] mr-10 `}
             >
               <img
                 src={slides[i].imageUrl}
