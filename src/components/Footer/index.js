46d8fcfcea64d9bae6b2a240acb97b9616fc09ac
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-26 23:33:46 +0700

    add href for about us

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -44,7 +44,7 @@ const Footer = () => {
               {/* <Text as="span" className="text-orange-700 bg-clip-text">
                 STUDIO
               </Text> */}
-              <Text as="span" className="bg-orange-700 bg-clip-text text-transparent">
+              <Text as="span" className="text-transparent bg-clip-text bg-orange-700">
                 STUDIO
               </Text>
             </Link>
