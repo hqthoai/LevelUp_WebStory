d8561a1b4913a30fd88e1b75ee5e046cb27d814f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-26 17:39:14 +0700

    update config

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,14 +2,15 @@ import ProductionCard from '../components/ProductionCard/ProductionCard';
 import GameComing from '../components/GameComming/GameComing';
 import TeamMembers from '../components/TeamMembers/TeamMembers';
 import ScrollToTopButton from '../components/ScrollToTop/ScrollToTopButton';
+import SlideImage from '../components/SlideImage/SlideImage';
 
 function HomePage() {
   return (
-    <div className="container bg-slate-800 min-h-[100vh]">
+    <div className="bg-slate-800 min-h-[100vh]">
       <ProductionCard />
       <GameComing />
-      <TeamMembers/>
-      <ScrollToTopButton/>
+      <TeamMembers />
+      <SlideImage />
     </div>
   );
 }
