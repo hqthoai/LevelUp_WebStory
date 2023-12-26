0a66387eb45f222647e54f66b101dc5fccc3fea3
Quoc Thoai <thoaifamily83@gmail.com>
2023-12-27 00:18:17 +0700

    fix production, game comming, add about us text component, delete babel

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AboutUsMainBanner from '~/components/MainBanner/AboutUsMainBanner.js';
+import AboutUsMainBanner from '../components/MainBanner/AboutUsMainBanner.js';
 
 const AboutUs = () => {
   return (
