c5a6275af4c3e6ca292d03b74f9e5bf4888b3b46
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-26 23:30:49 +0700

    add props

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AboutUsMainBanner from '~/components/MainBanner/AboutUsMainBanner.js';
 
-const AboutUs = () => {
+const AboutUs = ({title = "About Us"}) => {
   return (
     <div>
       <div className="bg-slate-800 min-h-[100vh] w-full">
-        <AboutUsMainBanner />
+        <AboutUsMainBanner title={title}/>
       </div>
     </div>
   );
