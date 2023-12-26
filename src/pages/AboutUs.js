9c5237ebcb4a838ec402af1cc0191662c061163f
kevin.nguyen <phanthanhluan553@gmail.com>
2023-12-27 00:08:08 +0700

    feat: add our mission

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutUsMainBanner from '~/components/MainBanner/AboutUsMainBanner.js';
+import OurMission from '~/components/OurMission';
 
 const AboutUs = () => {
   return (
     <div>
       <div className="bg-slate-800 min-h-[100vh] w-full">
         <AboutUsMainBanner />
+        <OurMission />
       </div>
     </div>
   );
