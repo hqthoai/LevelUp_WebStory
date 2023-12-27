b2789ccac85340d7ac78e2e63749fbc317ca55a2
kevin.nguyen <phanthanhluan553@gmail.com>
2023-12-27 23:49:27 +0700

    fix: fix backgound

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import AboutUs from './AboutUs'
-import TeamMembers from '../components/TeamMembers/TeamMembers'
-import MemOurTeam from '../components/MemOurTeam/MemOurTeam'
+import React from 'react';
+import AboutUs from './AboutUs';
+import TeamMembers from '../components/TeamMembers/TeamMembers';
+import MemOurTeam from '../components/MemOurTeam/MemOurTeam';
 function OurTeam() {
   return (
-    <div>
-      <AboutUs title='Our Team'/>  
-      <MemOurTeam/>
+    <div
+      style={{
+        backgroundImage:
+          'url("https://themebeyond.com/demo/haldalive/wp-content/uploads/2022/04/area_bg-scaled.jpg")',
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <AboutUs title="Our Team" />
+      <MemOurTeam />
     </div>
-  )
+  );
 }
 
-export default OurTeam
+export default OurTeam;
