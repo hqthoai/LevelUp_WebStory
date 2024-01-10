23cc197dcb2b44d9eed12ac99d07974d75c8b29f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2024-01-11 00:48:36 +0700

    re-make this component

--- src/components/OriginalProductionBusiness/OriginalProductionBusiness.js
+++ src/components/OriginalProductionBusiness/OriginalProductionBusiness.js
@@ -24,30 +24,31 @@ function OriginalProductionBusiness() {
                 boxShadow: '0 1px 12px 1px #ff8503c0',
               }}
             >
-              <div className="flex-col items-center justify-between mt-[-70px]">
+              <div className="mt-[24px] mb-12">
                 {/* title */}
-                <div className="text-white font-bold text-[24px] uppercase mb-[20px]">
+                <div className="text-[white] font-bold mt-[-48px] text-[24px] uppercase mb-[7px]">
                   Hot deal
                 </div>
                 {/* content */}
-                <div className="text-white text-[16px] mt-[-8px]  font-light font-pop w-[80%] text-justify ">
+                <div className="text-white text-[16px]  font-light font-pop w-[80%] text-justify">
                   Our commitment to innovation and efficiency allows us to break barriers by
                   lowering the cost of making games without compromising on quality. We believe that
                   great games should be accessible to all, and we're making that vision a reality.
                 </div>
                 {/* offer overlay */}
                 <div
-                  className="absolute right-0 top-[120px] -rotate-90 text-[#d98d7b] text-[40px] font-bold"
+                  className="absolute right-[-30px] top-[120px] -rotate-90 text-[#d98d7b] text-[40px] font-bold"
                   style={{
                     WebkitTextFillColor: 'transparent',
                     WebkitTextStrokeWidth: '1px',
                     WebkitTextStrokeColor: '#d98d7b',
                   }}
                 >
-                  OFFER
+                  BUSINESS
                 </div>
               </div>
             </div>
+
             <div
               className="w-[420px] h-[320px] flex items-center justify-around mr-8  px-[16px] relative
             bg-gradient-to-t from-[#f95249e3] from-40% via-[#ff6a00c7] via-80% to-[#ff8503c0] to-1%
@@ -58,29 +59,30 @@ function OriginalProductionBusiness() {
             >
               <div className="mt-[24px] mb-12">
                 {/* title */}
-                <div className="text-[white] font-bold text-[24px] uppercase mb-[7px]">
+                <div className="text-[white] font-bold mt-[-26px] text-[24px] uppercase mb-[7px]">
                   Discover
                 </div>
                 {/* content */}
                 <div className="text-white text-[16px]  font-light font-pop w-[80%] text-justify">
-                  We don't stop at Roblox and Steam. We can seamlessly integrate your game into
-                  websites and mobile apps, expanding your reach and player base. We work closely
-                  with you every step of the way, ensuring your vision is realized and your game
-                  succeeds in the competitive gaming market.
+                  We can seamlessly integrate your game into websites and mobile apps, expanding
+                  your reach and player base. We work closely with you every step of the way,
+                  ensuring your vision is realized and your game succeeds in the competitive gaming
+                  market.
                 </div>
                 {/* offer overlay */}
                 <div
-                  className="absolute right-0 top-[120px] -rotate-90 text-[#d98d7b] text-[40px] font-bold"
+                  className="absolute right-[-30px] top-[120px] -rotate-90 text-[#d98d7b] text-[40px] font-bold"
                   style={{
                     WebkitTextFillColor: 'transparent',
                     WebkitTextStrokeWidth: '1px',
                     WebkitTextStrokeColor: '#d98d7b',
                   }}
                 >
-                  OFFER
+                  BUSINESS
                 </div>
               </div>
             </div>
+
             <div
               className="w-[420px] h-[320px] flex items-center justify-around px-[16px] relative
             bg-gradient-to-t from-[#f95249e3] from-40% via-[#ff6a00c7] via-80% to-[#ff8503c0] to-1%
@@ -101,14 +103,14 @@ function OriginalProductionBusiness() {
                 </div>
                 {/* offer overlay */}
                 <div
-                  className="absolute right-0 top-[120px] -rotate-90 text-[#d98d7b] text-[40px] font-bold"
+                  className="absolute right-[-30px] top-[120px] -rotate-90 text-[#d98d7b] text-[40px] font-bold"
                   style={{
                     WebkitTextFillColor: 'transparent',
                     WebkitTextStrokeWidth: '1px',
                     WebkitTextStrokeColor: '#d98d7b',
                   }}
                 >
-                  OFFER
+                  BUSINESS
                 </div>
               </div>
             </div>
